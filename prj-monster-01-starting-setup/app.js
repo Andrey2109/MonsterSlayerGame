0698738c69b2a913fc102c4c0c91fa435e9f27e5
Andrey2109 <andrey@podiumcomp.com>
2024-10-05 14:25:46 +0300

    create data properties and methods to attack actions

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -1,4 +1,25 @@
+function randomDamage(min, max){
+    return Math.floor.Math.random() * (max -min) + min;
+}
+
 const app = Vue.createApp({
+    data(){
+        return{
+            playerHealth: 100,
+            monsterHealth: 100
+        }
+    },
+    methods: {
+        attackPlayer(){
+            let damage =randomDamage(5, 12)
+            this.playerHealth -= damage
+            this.attackMonster()
+        },
+        attackMonster(){
+            let damage = randomDamage(8, 15)
+            this.monsterHealth -= damage
+        }
+    }
 
 })
 
